816faf7ecbc91e11af6e0581bece69affc5e33a6
zeusone <1017794634@qq.com>
2015-10-21 23:13:34 +0800

    丰富了线形回归
    增加了随机梯度下降和最小二乘法,需要用到numpy库进行矩阵运算

--- pro/liner_reg.py
+++ pro/liner_reg.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def str_data(s):
     s = s.replace('\t', ' ')
     list_s = s.split()
@@ -10,27 +12,28 @@ def str_data(s):
 a = 0.0005     #set rate of down
 fp = open("../data/ex0.txt", "r")
 
-p = [0.0, 0.0, 0.0]
-pnew = [0.0, 0.0, 0.0]
+p = [0.0, 0.0]
+pnew = [0.0, 0.0]
 
 s = fp.readline()
 
+X = np.zeros((200, 2)) 
+Y = np.zeros((200, 1))
 list_d = [ [0] * 3 ] * 300
-s_x = [0.0, 0.0, 0.0]
-s_y = 0.0
 num = 0
 while len(s) > 0:
     list_d[num] = str_data(s)
-    s_x[0] += 1
-    s_x[1] += list_d[num][0]
-    s_x[2] += list_d[num][1]
-    s_y += list_d[num][2]
+    X[num][0] = list_d[num][0]
+    X[num][1] = list_d[num][1]
+    Y[num][0] = list_d[num][2]
     num += 1
     s = fp.readline()
 
 fp.close()
 
 ti = 0
+"""
+#批量梯度下降算法
 while 1:
     #print (su)
     ti += 1
@@ -39,16 +42,36 @@ while 1:
     su = 0.0
     for i in range(0, num):
         t1 = 0.0
-        t1 += p[0]
-        t1 += p[1] * list_d[i][0]
-        t1 += p[2] * list_d[i][1]
+        t1 += p[0] * list_d[i][0]
+        t1 += p[1] * list_d[i][1]
         t1 -= list_d[i][2]
         #print (t1)
-        pnew[0] -= (a * t1)
-        pnew[1] -= (a * t1 * list_d[i][0])
-        pnew[2] -= (a * t1 * list_d[i][1])
+        pnew[0] -= (a * t1 * list_d[i][0])
+        pnew[1] -= (a * t1 * list_d[i][1])
     #print (pnew)
-    for i in range(0, 3):
+    for i in range(0, 2):
         p[i] = pnew[i]
+"""
+"""
+#随机梯度下降算法不再需要pnew做中间结果
+while 1:
+    #print (su)
+    ti += 1
+    if ti > 500:
+        break
+    su = 0.0
+    for i in range(0, num):
+        t1 = 0.0
+        t1 += p[0] * list_d[i][0]
+        t1 += p[1] * list_d[i][1]
+        t1 -= list_d[i][2]
+        #print (t1)
+        p[0] -= (a * t1 * list_d[i][0])
+        p[1] -= (a * t1 * list_d[i][1])
+"""
+#解析方法，直接求解最小二乘解,需要利用x和y构造矩阵
 
+res = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
+
+print (res)
 print (p)
