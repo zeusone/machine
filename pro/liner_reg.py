bc7863a0998542f4d28ea7cc6312d4d0906e39f5
zeusone <1017794634@qq.com>
2015-10-30 23:49:33 +0800

    线性回归的改变
    把线性回归的代码改为了类和矩阵运算的形式,稍后会把所有的代码都更新为这种形式

--- pro/liner_reg.py
+++ pro/liner_reg.py
@@ -1,77 +1,83 @@
 import numpy as np
 
-def str_data(s):
-    s = s.replace('\t', ' ')
-    list_s = s.split()
-    list_dm = [0, 0, 0]
-    for i in range(0, 3):
-        list_dm[i] = float(list_s[i])
+class liner_reg:
+    __ma = 500
+    __a = 0.005#梯度下降速率
+    __learn_Y = np.zeros((__ma, 1))   #训练用Y矩阵
+    __right_Y = np.zeros((__ma, 1))  #预测用Y对应的真值
+    __pre_Y = np.zeros((__ma, 1))     #预测用Y矩阵,存储预测结果
 
-    return list_dm
+    def __init__(self, n):
+        self.__n = n
+        self.__learn_m = 0
+        self.__learn_X = np.zeros((self.__ma, self.__n + 1))
+        self.__pre_X = np.zeros((self.__ma, self.__n + 1))
+        self.__the = np.zeros((self.__n + 1, 1))
+        self.__the_new = np.zeros((self.__n + 1, 1))
+        self.__ti = 0
 
-a = 0.0005     #set rate of down
-fp = open("../data/ex0.txt", "r")
+        
+    def str_data(self, s):
+        s = s.replace('\t', ' ')
+        list_s = s.split()
+        list_dm = np.zeros((1, self.__n + 2))
+        for i in range(0, self.__n + 2):
+            list_dm[0][i] = float(list_s[i])
+        return list_dm
 
-p = [0.0, 0.0]
-pnew = [0.0, 0.0]
+    def data_solve(self):
+        fp = open("../data/ex0.txt", "r")
+        while 1:
+            s = fp.readline()
+            if len(s) == 0:
+                break
+            t1 = self.str_data(s)
+            self.__learn_X[self.__learn_m] = t1[0][0 : self.__n + 1]
+            self.__learn_Y[self.__learn_m] = t1[0][self.__n  + 1:]
+            self.__learn_m += 1
+        fp.close()
+        self.__pre_m = int(0.2 * self.__learn_m)    #20%的数据用来预测
+        self.__learn_m -= self.__pre_m              #余下的数据用来训练
+        for i in range(0, self.__pre_m):            #得到预测数据
+            self.__pre_X[i] = self.__learn_X[self.__learn_m + i]
+            self.__right_Y[i] = self.__learn_Y[self.__learn_m + i]
 
-s = fp.readline()
+    def gradient_descent(self):     #梯度下降算法
+        while 1:
+            self.__ti += 1
+            if self.__ti > 300:
+                break
+            self.__the_new = self.__the
+            for i in range(0, self.__learn_m):
+                t1 = 0.0
+                t1 += np.dot(self.__learn_X[i], self.__the)
+                t1 -= self.__learn_Y[i][0]
+                tx = np.transpose(np.array([self.__learn_X[i]]))  #这里矩阵的每一行只是一个列表或一维数组，不是一个矩阵
+                self.__the_new -= self.__a * t1 * tx
+            self.__the = self.__the_new
+        return self.__the
 
-X = np.zeros((200, 2)) 
-Y = np.zeros((200, 1))
-list_d = [ [0] * 3 ] * 300
-num = 0
-while len(s) > 0:
-    list_d[num] = str_data(s)
-    X[num][0] = list_d[num][0]
-    X[num][1] = list_d[num][1]
-    Y[num][0] = list_d[num][2]
-    num += 1
-    s = fp.readline()
+    def rand_gradient_descent(self): #随机梯度下降
+        while 1:
+            self.__ti += 1
+            if self.__ti > 500:
+                break
+            for i in range(0, self.__learn_m):
+                t1 = 0.0
+                t1 += np.dot(self.__learn_X[i], self.__the)
+                t1 -= self.__learn_Y[i][0]
+                tx = np.transpose(np.array([self.__learn_X[i]]))  #这里矩阵的每一行只是一个列表或一维数组，不是一个矩阵
+                self.__the -= self.__a* t1 * tx
+        return self.__the
+    def resolution(self):   #要使用解析方法，涉及的矩阵不能线性相关,所以要对矩阵进行处理
+        tx = self.__learn_X[0 : self.__learn_m]
+        ty = self.__learn_Y[0 : self.__learn_m]
+        self.__the = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(tx), tx)), np.transpose(tx)), ty)
+        return self.__the
 
-fp.close()
+liner = liner_reg(1)
 
-ti = 0
-"""
-#批量梯度下降算法
-while 1:
-    #print (su)
-    ti += 1
-    if ti > 1000:
-        break
-    su = 0.0
-    for i in range(0, num):
-        t1 = 0.0
-        t1 += p[0] * list_d[i][0]
-        t1 += p[1] * list_d[i][1]
-        t1 -= list_d[i][2]
-        #print (t1)
-        pnew[0] -= (a * t1 * list_d[i][0])
-        pnew[1] -= (a * t1 * list_d[i][1])
-    #print (pnew)
-    for i in range(0, 2):
-        p[i] = pnew[i]
-"""
-"""
-#随机梯度下降算法不再需要pnew做中间结果
-while 1:
-    #print (su)
-    ti += 1
-    if ti > 500:
-        break
-    su = 0.0
-    for i in range(0, num):
-        t1 = 0.0
-        t1 += p[0] * list_d[i][0]
-        t1 += p[1] * list_d[i][1]
-        t1 -= list_d[i][2]
-        #print (t1)
-        p[0] -= (a * t1 * list_d[i][0])
-        p[1] -= (a * t1 * list_d[i][1])
-"""
-#解析方法，直接求解最小二乘解,需要利用x和y构造矩阵
-
-res = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
-
-print (res)
-print (p)
+liner.data_solve()
+#print (liner.gradient_descent())
+print (liner.rand_gradient_descent())
+#print (liner.resolution())
