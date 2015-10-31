d45d3e4259302d74388899cd8e93a3fed781d057
zeusone <1017794634@qq.com>
2015-10-31 22:51:44 +0800

    多项式回归
    多项式回归转换为矩阵运算和类的形式

--- pro/softmax.py
+++ pro/softmax.py
@@ -4,125 +4,125 @@
 import numpy as np
 import math
 
-n = 4
-k = 3
+class softmax:
+    __fdict1 = {'Iris-setosa':1, 'Iris-versicolor':2, 'Iris-virginica':3}
+ #训练用数据
+    __learn_m = 0
+    __pre_m = 0
+    __mam = 500
+    __learn_Y = np.zeros((__mam, 1))
+    __r = 0.05
 
-#参数是一个k*(n+1)的矩阵,每一行对应一个概率值的参数
-def f_1(a, b):
-    if a == b :
-        return 1
-    return 0
+#预测用数据
+    __pre_right = np.zeros((__mam, 1))
+    __predic_Y = np.zeros((__mam, 1))
+    def __init__(self, n, k):
+        self.__n = n
+        self.__k = k
+        self.__learn_X = np.zeros((self.__mam, self.__n + 1))
+        self.__pre_X = np.zeros((self.__mam, self.__n + 1))
+        self.__cans = np.zeros((self.__k, self.__mam))
+        self.__th = np.zeros((self.__k, (self.__n + 1)))
+        self.__cans = np.zeros((self.__k, self.__mam))
+    def f_1(self, a, b):
+        if a == b :
+            return 1
+        return 0
 
-#假设函数，参数th为k*(n+1)的参数矩阵，x为输入变量
-def hf(th, x):
-    ans = np.zeros((k, 1))
-    s1 = 0
-    tt = np.zeros((k, 1))
-    for i in range(0, k):
-        tt[i] = np.dot(th[i], x)
-    ma = tt[0]
-    for i in range(k):
-        if(tt[i] > ma):
-            ma = tt[i]
-    for i in range(k):
-        tt[i] -= ma
-        ans[i][0] = math.exp(tt[i])
-        s1 += math.exp(tt[i])
-    ans = ans / s1
+    def ori_data(self, s):
+        list_s = s.split(',')
+        list_d = [0] * 6
+        list_d[0] = 1.0
+        for i in range(0, self.__n):
+            list_d[i + 1] = float(list_s[i])
+        list_d[self.__n + 1] = self.__fdict1[list_s[self.__n]]
+        return list_d
 
-    return ans
+
+    def file_in(self):
+        fp = open("../data/iris.txt")
+        for i in range(0, 3):
+            for j in range(0, 40):
+                s = fp.readline() 
+                s = s.strip('\n')
+                list_d = self.ori_data(s)
+                self.__learn_X[self.__learn_m] = list_d[0 : self.__n + 1]
+                self.__learn_Y[self.__learn_m] = list_d[self.__n + 1 :]
+                self.__learn_m += 1
+            for j in range (0, 10):
+                s = fp.readline()
+                s = s.strip('\n')
+                list_d = self.ori_data(s)
+                self.__pre_X[self.__pre_m] = list_d[0 : self.__n + 1]
+                self.__pre_right[self.__pre_m] = list_d[self.__n + 1 :]
+                self.__pre_m += 1
+        fp.close()
+
+
+    def hf(self, x):
+        ans = np.zeros((self.__k, 1))
+        s1 = 0
+        tt = np.zeros((self.__k, 1))
+        for i in range(0, self.__k):
+            tt[i] = np.dot(self.__th[i], x)
+        ma = tt[0]
+        for i in range(self.__k):
+            if(tt[i] > ma):
+                ma = tt[i]
+        for i in range(self.__k):
+            tt[i] -= ma
+            ans[i][0] = math.exp(tt[i])
+            s1 += math.exp(tt[i])
+        ans = ans / s1
+        return ans
+    def gradient_descent(self):     #梯度下降算法
+        ti = 0
+        while 1:
+            if ti >= 500:
+                break
+            ti += 1
+            for i in range(0, self.__learn_m):
+                tx = np.transpose(self.__learn_X[i])
+                tans = self.hf(tx)
+                for j in range(0, self.__k):
+                    self.__cans[j][i] = tans[j][0]
+            for j in range(0, self.__k):
+                dth = np.zeros((1, (self.__n + 1)))
+                for i in range(0, self.__learn_m):
+                    dth = dth + self.__learn_X[i] * (self.f_1(self.__learn_Y[i][0], j + 1) - self.__cans[j][i])
+                dth = dth / self.__learn_m
+                dth = -1 * dth
+                dth += 0.01 * self.__th[j]
+                self.__th[j] = self.__th[j] -  self.__r * dth
+
+    def predict(self):
+        right = 0
+        for i in range(30):
+            tans = self.hf(np.transpose(self.__pre_X[i]))
+            ma = 0
+            mi = -1
+            for j in range(self.__k):
+                if tans[j][0] > ma:
+                    ma = tans[j][0]
+                    mi = j
+            mi += 1
+            if mi == self.__pre_right[i]:
+                right += 1
+        print (right)
+
+
 
 #数据的读入处理
 
-fdict1 = {'Iris-setosa':1, 'Iris-versicolor':2, 'Iris-virginica':3}
-def ori_data(s):
-    list_s = s.split(',')
-    list_d = [0] * 6
-    list_d[0] = 1.0
-    for i in range(0, 4):
-        list_d[i + 1] = float(list_s[i])
-    list_d[5] = fdict1[list_s[4]]
-    
-    return list_d
 
-fp = open("../data/iris.txt")
 
-#训练用数据
-learnx = np.zeros((120, 5))
-learny = np.zeros((120, 1))
-
-#预测用数据
-predicx = np.zeros((30, 5))
-pre_right = np.zeros((30, 1))
-predicy = np.zeros((30, 1))
-
-#数据读入
-for i in range(0, 3):
-    for j in range(0, 40):
-        s = fp.readline() 
-        s = s.strip('\n')
-        list_d = ori_data(s)
-        for l in range(0, 5):
-            learnx[i * 40 + j][l] = list_d[l]
-        learny[i * 40 + j][0] = list_d[5]
-    for j in range (0, 10):
-        s = fp.readline()
-        s = s.strip('\n')
-        list_d = ori_data(s)
-        for l in range(0, 5):
-            predicx[i * 10 + j][l] = list_d[l]
-        pre_right[i * 10 + j][0] = list_d[5]
-
-fp.close()
 
 
 #参数矩阵
-th = np.zeros((k, (n + 1)))
-r = 0.05
 #导数矩阵
-
-ti = 0
-m = 120
-
-cans = np.zeros((k, m))
-while 1:
-    if ti % 1000 == 0:
-        print (th)
-    if ti >= 500:
-        break
-    ti += 1
-
-    for i in range(0, m):
-        tx = np.transpose(learnx[i])
-        tans = hf(th, tx)
-        for j in range(0, k):
-            cans[j][i] = tans[j][0]
-    #print (cans)
-    for j in range(0, k):
-        dth = np.zeros((1, (n + 1)))
-        for i in range(0, m):
-            dth = dth + learnx[i] * (f_1(learny[i][0], j + 1) - cans[j][i])
-        dth = dth / m
-        dth = -1 * dth
-        dth += 0.01 * th[j]
-        th[j] = th[j] -  r * dth
-
-print (th)
-
 #开始进行预测
 
-right = 0
-for i in range(30):
-    tans = hf(th, np.transpose(predicx[i]))
-    ma = 0
-    mi = -1
-    for j in range(3):
-        if tans[j][0] > ma:
-            ma = tans[j][0]
-            mi = j
-    mi += 1
-    if mi == pre_right[i]:
-        right += 1
-
-
-print (right)
+soft = softmax(4, 3)
+soft.file_in()
+soft.gradient_descent()
+soft.predict()
