cd699981e01d8c696d906ab7708acf4344683397
zeusone <1017794634@qq.com>
2015-11-11 22:11:28 +0800

    朴素贝叶斯
    增加了朴素贝叶斯的程序,并且添加了拉普拉斯平滑，分别写了X取0，1和0~k的程序，Y的取值都是多值

--- pro/naive_bayes.py
+++ pro/naive_bayes.py
@@ -4,9 +4,72 @@
 #第二种假设第i个邮件的长度为ni,xi=k表示该邮件第i个词为编号为k
 
 import numpy as np
-def naive_bayes:
+import math
+class naive_bayes:
     __m = 500
+    __N = 50000
     __learn_Y = np.zeros((__m, 1))
     __pre_Y = np.zeros((__m, 1))
     __learn__len = np.zeros((__m, 1))   #每一个学习样本的长度
 #fdict[] = 词典内容，可以自行读入,这里假设已经记录了每一个邮件的向量X,实际情况中可以用文件加映射的方式读入，这里只写方法
+    __learn_X = np.zeros((__m, __N))
+
+    def f_1(self, a, b):
+        if a == b:
+            return 1
+        return 0
+
+    def __init__(self, k):
+        self.__k = k #设定Y的取值范围为0-k-1，是为了能够更好的扩展性
+        self.__p = np.zeros((self.__k, 1))  #P(y=k)
+        self.__px_y = np.zeros((self.__k, self.__N)) #P(xi|y = i)或P(j|y=i)
+
+    def train_Bemoulli(self): #按照二元伯努利的形式训练，x只有0和1两种情况
+        for i in range (0, self.__m):
+            self.__p[self.__learn_Y[i][0]][0] += 1
+            for j in range(0, self.__N):
+                if self.f_1(self.__learn_X[i][j], 1) == 1 :
+                    self.__px_y[self.__learn_Y[i][0]][j] += 1
+        for i in range(0, self.__k):
+            for j in range(0, self.__N):
+                self.__px_y[i][j] = (self.__px_y[i][j] + 1.0) / (self.__p[i][0] + 2.0) #加拉普拉斯平滑
+            self.__p[i][0] /= self.__m
+
+    def train_mul_Bemoulli(self): #按照多元伯努利，x取值范围为0-N-1
+        total_Y = np.zeros((self.__k, 1))
+        for i in range(0, self.__m):
+            self.__p[self.__learn_Y[i][0]][0] += 1
+            self.__total_Y[self.__learn_Y[i][0]][0] += self.__learn_len[i][0]
+            for j in range(0, self.__learn_len[i][0]):
+                self.__px_y[self.__learn_Y[i][0]][self.__learn_X[i][j]] += 1
+        for i in range(0, self.__k):
+            for j in range(0, self.__N):
+                self.__px_y[i][j] = (self.__px_y[i][j] + 1.0) / (total_Y[i][0] + self.__N)
+            self.__p[i][0] /= self.__m
+
+    def predict_bem(self, x):   #预测只有两种情况的x
+        pre = self.__p
+        for i in range(0, self.__N):
+            if x[i] == 1:
+                for j in range(0, self.__k):
+                    pre[j][0] *= self.__px_y[j][i]
+        ans = -1
+        ma = 0
+        for i in range(0, self.__k):
+            if pre[i][0] > ma:
+                ans = i
+                ma = pre[i][0]
+        return ans + 1
+    
+    def predict_mul_bem(self, x, ni):   #预测X多种取值下的概率
+        pre = self.__p
+        for i in range(0, ni):
+            for j in range(0, self.__k):
+                pre[j][0] *= self.__px_y[j][x[i]]
+        ans = -1
+        ma = 0
+        for i in range(0, self.__k):
+            if pre[i][0] > ma:
+                ans = i
+                ma = pre[i][0]
+        return ans + 1
